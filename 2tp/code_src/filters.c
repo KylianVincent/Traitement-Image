f54622ef98f31c18e1c10436f09de5379f8a530c
Thomas Loïodice <thomas.loiodice@ensimag.grenoble-inp.fr>
2017-03-31 14:18:06 +0200

    oups merge again

--- 2tp/code_src/filters.c
+++ 2tp/code_src/filters.c
@@ -44,7 +44,31 @@ void adaptativeFilterRecursion(double** imSrc, double** imRes, double k, int nl,
             vm1 = prolongateByMirror(v-1, nc);
             vp1 = prolongateByMirror(v+1, nc);
 
-<<<<<<< HEAD
+            //printf("%d - %d - %d - %d\n", um1, up1, vm1, vp1);
+
+            omega[u][v]=exp(-(pow(imSrc[up1][v] - imSrc[um1][v], 2.0) + pow(imSrc[u][vp1] - imSrc[u][vm1], 2.0))/(2*pow(k, 2.0)));
+        }
+    }
+
+    //Image computation
+    //For each pixel
+    for (int u = 0; u<nl; u++) {
+        for (int v = 0; v<nc; v++) {
+            num = 0;
+            denom = 0;
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    num += omega[prolongateByMirror(u+i, nl)][prolongateByMirror(v+j, nc)] * imSrc[prolongateByMirror(u+i, nl)][prolongateByMirror(v+j, nc)];
+                    denom += omega[prolongateByMirror(u+i, nl)][prolongateByMirror(v+j, nc)];
+                }
+            }
+            imRes[u][v] = num/denom;
+        }
+    }
+}
+
+/* ----------- Filtre bilatéral ----------*/
+
 void bilateralFilter(double** sortie, double** entree, int nl, int nc, int sigma1) { 
 	int x,y,i,j,ii,jj,k,l=(1+6*sigma1),pixelNorme;
 	double gaussienne[l*l+1];
@@ -80,64 +104,6 @@ void bilateralFilter(double** sortie, double** entree, int nl, int nc, int sigma
 			sortie[x][y] /= pixelNorme;
 		}
 	}
-=======
-            //printf("%d - %d - %d - %d\n", um1, up1, vm1, vp1);
-
-            omega[u][v]=exp(-(pow(imSrc[up1][v] - imSrc[um1][v], 2.0) + pow(imSrc[u][vp1] - imSrc[u][vm1], 2.0))/(2*pow(k, 2.0)));
-        }
-    }
-
-    //Image computation
-    //For each pixel
-    for (int u = 0; u<nl; u++) {
-        for (int v = 0; v<nc; v++) {
-            num = 0;
-            denom = 0;
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    num += omega[prolongateByMirror(u+i, nl)][prolongateByMirror(v+j, nc)] * imSrc[prolongateByMirror(u+i, nl)][prolongateByMirror(v+j, nc)];
-                    denom += omega[prolongateByMirror(u+i, nl)][prolongateByMirror(v+j, nc)];
-                }
-            }
-            imRes[u][v] = num/denom;
-        }
-    }
->>>>>>> 27533f968fab9338c544cc62e271c30e3e920bb3
-}
-
-/* ----------- Filtre bilatéral ----------*/
-
-//void bilateralFilter(double** sortie, double** entree, int nl, int nc, int sigma1) {
-//	int x,y,i,j,pixelNorm;
-//	double gaussienne[pow((1+6*sigma1),2)];
-//    if (sortie==NULL) sortie=alloue_image_double(nl,nc);
-//	//Pour chaque pixel du masque(sigma) -> précalcule de la gaussienne
-//	for(i=-3*sigma1; i<3*sigma1; i++) {
-//		for(j=-3*sigma1; j<3*sigma1; j++) {
-//			//TODO calcul gaussienne
-//		}
-//	}
-//	//Pour tout les pixels de l'image
-//    for(y=1; y<nl-1; y++) {
-//        for(x=1; x<nc-1; x++) {
-//			pixelNorm=0;
-//			//Pour chaque pixel du masque(sigma)
-//			for(i=-3*sigma1; i<3*sigma1; i++) {
-//				for(j=-3*sigma1; j<3*sigma1; j++) {
-//					//TODO calcul sortit + pixelNorme
-//					/*
-//            sortie[i][j]=pow(entree[i+1][j+1]+entree[i][j+1]+entree[i-1][j+1]-(entree[i+1][j-1]+entree[i][j-1]+entree[i-1][j-1]),2);
-//            sortie[i][j]+=pow(entree[i+1][j-1]+entree[i+1][j]+entree[i+1][j+1]-(entree[i-1][j-1]+entree[i-1][j]+entree[i-1][j+1]),2);
-//            sortie[i][j]=sqrt(sortie[i][j]);
-//			*/
-//				}
-//			}
-//			//Normalisation
-//			sortie[x][y] /= pixelNorm;
-//		}
-//	}
-//}
-
 
 /* ------------- Filtre patch ------------*/
 
