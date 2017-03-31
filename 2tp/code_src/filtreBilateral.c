315d9bb6bd58464dc612404496eb3df11abd3a61
Kylian VINCENT <kylian.vincent@ensimag.grenoble-inp.fr>
2017-03-31 14:49:58 +0200

    Corection segfault programme main bilateral

--- 2tp/code_src/filtreBilateral.c
+++ 2tp/code_src/filtreBilateral.c
@@ -1,25 +1,28 @@
-
 #include "pgm.h"
 #include "filters.h"
+#include <math.h>
+/*
+    Filtrage linéaire résursif
+    S'utilise sous la forme "filtreAdaptatifRecursif source resultat k"
+ */
 
 int main (int ac, char **av) {  /* av[1] contient le nom de l'image, av[2] le nom du resultat . */
-  int nb,nl,nc, oldnl,oldnc, sigma;
-  unsigned char **im2=NULL,** im1=NULL;
-  double** im4,** im5, ** im6, ** im7, **im8, **im9,**im10;
+    int nl,nc;
+    unsigned char ** im1=NULL;
+    double** im4;
 
-  if (ac < 3) {printf("Usage : %s entree sortie sigma\n",av[0]); exit(1); }
-	/* Lecture d'une image pgm dont le nom est passe sur la ligne de commande */
-  im1=lectureimagepgm(av[1],&nl,&nc);
-  if (im1==NULL)  { puts("Lecture image impossible"); exit(1); }
+    if (ac < 4) {
+        printf("Usage : %s entree sortie k\n", av[0]); exit(1);
+    }
+    /* Lecture d'une image pgm dont le nom est passe sur la ligne de commande */
+    im1=lectureimagepgm(av[1],&nl,&nc);
+    if (im1==NULL)  { puts("Lecture image impossible"); exit(1); }
+    /* Calcul de son inverse video */
+    double**im3=imuchar2double(im1,nl,nc);
+    im4=alloue_image_double(nl,nc);
 
-  sigma = av[2];
-	/* transfomation en double -> calcul */ 
-	im5 = imuchar2double(im1,nl,nc);
-	/* Calcul des contours */
-	bilateralFilter(im4,im5,nl,nc,1);
+    //Filtrage
+    bilateralFilter(im4, im3, nl, nc, 1);
 
-	/* transfomation en char -> sauvgade*/ 
-	im2=imdouble2uchar(im4,nl,nc);
-	/* Sauvegarde dans un fichier dont le nom est passe sur la ligne de commande */
-  ecritureimagepgm(av[2],im2,nl,nc);
+    ecritureimagepgm(av[2],crop(imdouble2uchar(im4,nl,nc),0,0,nl,nc),nl,nc);
 }
